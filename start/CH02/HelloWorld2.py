a6c33bb1f475fefdc48f95945570395b0d3ebec0
Aaron5402 <aarona96@yahoo.com>
2022-04-17 20:53:26 -0700

    Happy Birthday

--- start/CH02/HelloWorld2.py
+++ start/CH02/HelloWorld2.py
@@ -5,3 +5,8 @@
 # Suggestion, build out 1 line at a time
 # Once multiple print statemetns exist, put a breakpoint at first print line
 # Then walk through as an example of "debugging"
+user_name = input("What is your name?")
+
+#Print Hello and their name
+print("Hello {0}".format(user_name))
+print("You are doing great today!")
